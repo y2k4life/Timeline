80254e2905656e2777a37ae433640f33a2d9f428
y2k4life <dotat@hotmail.com>
2020-04-19 11:12:52 -0700

    Change applying events from implicit to explicit
    Remove the "black magic" in an Aggregate by explicitly stating through generics what the AggregateState is. When calling the Apply on the Aggregate pass in the method that will be called to perform the action. Keep the implicit methods used when the Aggregate is loaded from the event store.

--- Sample.Domain/Aggregates/TransferAggregate.cs
+++ Sample.Domain/Aggregates/TransferAggregate.cs
@@ -4,26 +4,31 @@ using Timeline.Events;
 
 namespace Sample.Domain
 {
-    public class TransferAggregate : AggregateRoot
+    public class TransferAggregate : AggregateRoot<Transfer>
     {
-        public override AggregateState CreateState() => new Transfer();
+        public TransferAggregate()
+        {
+            State = new Transfer();
+        }
+        
+        //public override AggregateState CreateState() => _state;
 
         public void StartTransfer(Guid fromAccount, Guid toAccount, decimal amount)
         {
-            var e = new TransferStarted(fromAccount, toAccount, amount);
-            Apply(e);
+            var transferStartedEvent = new TransferStarted(fromAccount, toAccount, amount);
+            Apply(transferStartedEvent, State.When);
         }
 
         public void UpdateTransfer(string activity)
         {
             var e = new TransferUpdated(activity);
-            Apply(e);
+            Apply(e, State.When);
         }
 
         public void CompleteTransfer()
         {
             var e = new TransferCompleted();
-            Apply(e);
+            Apply(e, State.When);
         }
     }
 }
